6c941b17b15a0c1b8a3a14d183c4b9ddd5ae30e3
NikYurchik <14urka@gmail.com>
2023-09-26 18:28:05 -0700

    del

--- BotAssistant/Python/bot.py
+++
@@ -1,7 +0,0 @@
-from bot_assistant import Bot_assistant
-from userinterface import ConsoleInterface
-
-if __name__ == "__main__":
-    interface = ConsoleInterface()
-    ba = Bot_assistant(interface)
-    ba.main()
